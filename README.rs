62662d6b2c68448ce547599c175027bc0b96b983
Wilson Guo <wolfiethehusky@gmail.com>
2023-08-14 23:12:00 -0400

    ripped some socket code from this website

--- README.rs
+++ README.rs
@@ -1,1 +1,13 @@
 #hope this works
+#$ rustc client.rs; rustc server.rs
+
+# Terminal 1
+#$ ./server
+#Server started, waiting for clients
+
+# Terminal 2
+#$ ./client hello
+
+# Terminal 1
+#Server started, waiting for clients
+#Client said: hello
