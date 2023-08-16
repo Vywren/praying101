81f0f9856f3aa049bb1a175e2fc4da28f3a9a482
Wilson Guo <wolfiethehusky@gmail.com>
2023-08-16 16:20:05 -0400

    updated to comment

--- README.rs
+++ README.rs
@@ -1,13 +1,13 @@
-#hope this works
-#$ rustc client.rs; rustc server.rs
+/*hope this works
+$ rustc client.rs; rustc server.rs
 
 # Terminal 1
-#$ ./server
-#Server started, waiting for clients
+$ ./server
+Server started, waiting for clients
 
 # Terminal 2
-#$ ./client hello
+$ ./client hello
 
 # Terminal 1
-#Server started, waiting for clients
-#Client said: hello
+Server started, waiting for clients
+Client said: hello*/
